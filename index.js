50c85a03dceb77276a1979376ad1599c56c339f9
Emmanuel Akinlosotu <73029515+emanakin@users.noreply.github.com>
2022-10-17 20:26:58 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+var buttons = document.body.querySelectorAll(".drum");
+
+function handelClick(key){
+    
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3")
+            tom1.play();
+            break;
+
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3")
+            tom2.play();
+            break;
+        
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3")
+            tom3.play();
+            break;
+
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3")
+            tom4.play();
+            break;
+
+        case "j":
+            var snare = new Audio("sounds/snare.mp3")
+            snare.play();
+            break;
+
+        case "k":
+            var crash = new Audio("sounds/crash.mp3")
+            crash.play();
+            break;
+
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3")
+            kick.play();
+            break;
+
+        default:
+            console.log("Error")
+            break;
+    }
+}
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(){
+        handelClick(this.innerHTML);
+        btnAnimation(this.innerHTML)
+    });
+}
+
+// listens for key board presses
+
+document.addEventListener("keydown", function(event){
+    // in the event that one is pressed pass the event.key propty to the handel click funciton
+    handelClick(event.key);
+    btnAnimation(event.key);
+});
+
+function btnAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed")
+    }, 100)
+
+}
